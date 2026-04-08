274c888abbe82513c327291e44b137da971043a5
premkumarmk0418-star <premkumarmk0418@gmail.com>
2026-04-08 18:44:07 +0530

    JavaLoops update

---
+++ src/JavaLoops/EnhancedLoop.java
@@ -0,0 +1,20 @@
+package JavaLoops;
+
+public class EnhancedLoop {
+    public static void main(String[] args) {
+        String[] candidateNames = {"prem","kumar","sanjay","naveen"};
+
+        for(String name : candidateNames){
+            System.out.println("names====>" + name);
+        }
+
+        System.out.println("------------------------------------");
+
+        int[] numbers = {100,400,500,600,800,900};
+
+        for(int item : numbers){
+            System.out.println("item===>" + item);
+        }
+    }
+}
+
