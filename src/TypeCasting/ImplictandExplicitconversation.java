5d6a0f4d6df4066b767537444240d54430e7e74e
premkumarmk0418-star <premkumarmk0418@gmail.com>
2026-04-08 18:22:10 +0530

    TypeCasting update

---
+++ src/TypeCasting/ImplictandExplicitconversation.java
@@ -0,0 +1,17 @@
+package TypeCasting;
+
+public class ImplictandExplicitconversation {
+    public static void main(String[] args) {
+
+        int a = 200;
+        double result = a;
+        System.out.println("result====>" + result);
+
+        double value = 2000;
+        float floatResult = (float) value;
+        System.out.println("float Result====>" + floatResult);
+
+
+    }
+}
+
